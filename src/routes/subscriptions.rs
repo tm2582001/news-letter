03f9098f53f53492f71c7a1d8009dd0e96c14e2a
Tushar Mehta <tm2582001@gmail.com>
2025-05-18 22:26:56 +0530

    added logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, HttpResponse};
-use sqlx::PgPool;
 use chrono::Utc;
+use sqlx::PgPool;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -9,10 +9,16 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(
-    form: web::Form<FormData>,
-    db_pool: web::Data<PgPool>,
-) -> HttpResponse {
+pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    
+    let request_id = Uuid::new_v4();
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    tracing::info!("request_id {} - Saving new subscriber details in database", request_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -22,11 +28,16 @@ pub async fn subscribe(
         form.email,
         form.name,
         Utc::now()
-    ).execute(db_pool.get_ref())
-    .await{
-        Ok(_)=> HttpResponse::Ok().finish(),
-        Err(e)=>{
-            println!("Failed to execute query: {}", e);
+    )
+    .execute(db_pool.get_ref())
+    .await
+    {
+        Ok(_) => {
+            tracing::info!("request_id {} - New subscriber details in the database", request_id);
+            HttpResponse::Ok().finish()
+        }
+        Err(e) => {
+            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,9 @@ use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
+
+use env_logger::Env;
 
 use crate::routes::{health_check, subscribe};
 
@@ -10,11 +13,16 @@ pub fn run(listener: TcpListener,
     db_pool: PgPool
 ) -> Result<Server, std::io::Error> {
 
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Wrap the connection in a smart pointer
     let db_pool = web::Data::new(db_pool);
 
     let server = HttpServer::new(move|| {
         App::new()
+            // Middlewares are added using the `wrap` methon on `App`
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
