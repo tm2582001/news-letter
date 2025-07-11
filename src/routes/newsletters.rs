f3d17774b3f293df53d9d833e6c0d30747d73338
Tushar Mehta <tm2582001@gmail.com>
2025-07-11 12:51:15 +0530

    added logic to check basic authentication

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,9 +1,12 @@
 use crate::domain::SubscriberEmail;
 use crate::email_clients::EmailClient;
 use crate::routes::error_chain_fmt;
-use actix_web::http::StatusCode;
-use actix_web::{web, HttpResponse, ResponseError};
+use actix_web::http::header::{HeaderMap, HeaderValue};
+use actix_web::http::{StatusCode, header};
+use actix_web::{web, HttpRequest, HttpResponse, ResponseError};
 use anyhow::Context;
+use base64::Engine;
+use secrecy::SecretString;
 use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
@@ -20,6 +23,8 @@ pub struct Content {
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
@@ -31,9 +36,27 @@ impl std::fmt::Debug for PublishError {
 }
 
 impl ResponseError for PublishError {
-    fn status_code(&self) -> StatusCode {
+    // fn status_code(&self) -> StatusCode {
+    //     match self {
+    //         PublishError::AuthError(_) => StatusCode::UNAUTHORIZED,
+    //         PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    //     }
+    // }
+
+    fn error_response(&self) -> HttpResponse {
         match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            PublishError::UnexpectedError(_)=>{
+                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+            PublishError::AuthError(_)=>{
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
+
+                response.headers_mut()
+                    .insert(header::WWW_AUTHENTICATE, header_value);
+
+                response
+            }
         }
     }
 }
@@ -42,12 +65,14 @@ pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
+    request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
+    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
-
         match subscriber {
-            Ok(subscriber)=>{
+            Ok(subscriber) => {
                 email_client
                     .send_email(
                         &subscriber.email,
@@ -56,9 +81,11 @@ pub async fn publish_newsletter(
                         &body.content.text,
                     )
                     .await
-                    .with_context(|| format!("Falied to send newsletter issue to {}", subscriber.email))?;
-            },
-            Err(error)=>{
+                    .with_context(|| {
+                        format!("Falied to send newsletter issue to {}", subscriber.email)
+                    })?;
+            }
+            Err(error) => {
                 tracing::warn!(
                     // We record the error chain as structured field on the log record.
                     error.cause_chain = ?error,
@@ -67,9 +94,51 @@ pub async fn publish_newsletter(
                 )
             }
         }
-
     }
     Ok(HttpResponse::Ok().finish())
+}
+
+struct Credetials {
+    username: String,
+    password: SecretString,
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credetials, anyhow::Error> {
+    // The header value if present, must be a valid UTF8 string
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF8 string")?;
+
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("The authorization sceme was not 'Basic'.")?;
+
+    let decoded_bytes = base64::engine::general_purpose::STANDARD
+        .decode(base64encoded_segment)
+        .context("Faled to bas64-decode 'Basic' credentials.")?;
+
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credential string is not valid UTF8.")?;
+
+    // Split into two segments, using ':' as delimiter
+    let mut credentials = decoded_credentials.splitn(2, ":");
+
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
+        .to_string();
+
+    Ok(Credetials {
+        username,
+        password: SecretString::new(password.into()),
+    })
 }
 
 struct ConfirmedSubscriber {
@@ -80,19 +149,17 @@ struct ConfirmedSubscriber {
 async fn get_confirmed_subscribers(
     pool: &PgPool,
     //* See https://sled.rs/errors.html for more information about this vector error handling
-) -> Result<Vec <Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
-
-    let confirmed_subscribers = sqlx::query!(
-        r#"SELECT email FROM subscriptions WHERE status = 'confirmed'"#
-    )
-    .fetch_all(pool)
-    .await?
-        .into_iter()
-        .map(|r| match SubscriberEmail::parse(r.email) {
-            Ok(email) => Ok(ConfirmedSubscriber { email }),
-            Err(error) => Err(anyhow::anyhow!(error))
-        })
-        .collect();
+) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
+    let confirmed_subscribers =
+        sqlx::query!(r#"SELECT email FROM subscriptions WHERE status = 'confirmed'"#)
+            .fetch_all(pool)
+            .await?
+            .into_iter()
+            .map(|r| match SubscriberEmail::parse(r.email) {
+                Ok(email) => Ok(ConfirmedSubscriber { email }),
+                Err(error) => Err(anyhow::anyhow!(error)),
+            })
+            .collect();
 
     Ok(confirmed_subscribers)
 }
